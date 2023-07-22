d49a8203bf2eab8e0a60c696d3dfc4d3be07660d
davidpinkevich <davidpinkevich95@gmail.com>
2023-07-22 13:24:00 +0300

    refactor: change path for App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import { Provider } from 'react-redux';
+import App from './components/App/App';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
