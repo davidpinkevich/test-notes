63675a1c7bb91cc0873051780ef622f12c4dd953
davidpinkevich <davidpinkevich95@gmail.com>
2023-07-22 13:24:21 +0300

    feat: add main page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header/Header';
+import { CreateNote } from '../../components/CreateNote/CreateNote';
+
+export const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <CreateNote />
+    </>
+  );
+};
